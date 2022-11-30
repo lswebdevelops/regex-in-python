4f8240f7b555a99fbddb752d523f79bd7a53f60c
lswebdevelops <sardanhaluciano9690@gmail.com>
2022-11-30 12:16:06 -0300

    adding regex to accept double names with dots.

--- regex.py
+++ regex.py
@@ -2,8 +2,8 @@
 #  for now it is just working on the python terminal, not as a script
 import re
 def redo_name(name):
-    result = re.search(r"^(\w*), (\w*)$", name)
+    result = re.search(r"^([\w \.-]*), ([\w \.-]*)$", name)
     if result is None:
             return name
     return "{} {}".format(result[2], result[1])
-print(redo_name("Sardanha, Luciano"))
+print(redo_name("Sardanha, Luciano F."))
