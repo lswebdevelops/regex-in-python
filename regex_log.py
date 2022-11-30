5f7eee63b177fec5c381627906c16416fc201d78
lswebdevelops <sardanhaluciano9690@gmail.com>
2022-11-30 15:40:06 -0300

    using PID to access any logs, with or without numbers

--- regex_log.py
+++ regex_log.py
@@ -7,4 +7,16 @@ regex = r"\[(\d+)\]"
 result = re.search(regex, log)
 print(result[0])
 print(result[1])
+# create a function to be used in case we don't have numbers between ()
+
+log1 = "July 31 07:51:48 mycomputer bad_process[34567]: ERROR Performing package upgrade"
+def extract_pid(log_line):
+    regex = r"\[(\d+)]"
+    result  = re.search(regex, log_line)
+    if result is None:
+        return ""
+    return result[1]
+print(extract_pid(log1))
+
+
 
