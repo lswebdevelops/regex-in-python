da13b703513f829101d1ad313acf60d0540def55
lswebdevelops <sardanhaluciano9690@gmail.com>
2022-11-30 15:32:07 -0300

    using regex to loop tru a log

---
+++ regex_log.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import re
+
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+print(result[0])
+print(result[1])
+
