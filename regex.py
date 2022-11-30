d8a98f60bde90bb2163b055d8a95b9f15187f71c
lswebdevelops <sardanhaluciano9690@gmail.com>
2022-11-30 12:14:46 -0300

    reorganize names

---
+++ regex.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+#  for now it is just working on the python terminal, not as a script
+import re
+def redo_name(name):
+    result = re.search(r"^(\w*), (\w*)$", name)
+    if result is None:
+            return name
+    return "{} {}".format(result[2], result[1])
+print(redo_name("Sardanha, Luciano"))
